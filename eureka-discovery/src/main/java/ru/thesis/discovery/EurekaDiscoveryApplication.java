fc6f24047c2b98829f124f5e166136b5401aea56
Iliya Pinyaskin <pinyaskiniliya163@gmail.com>
2024-12-17 13:16:30 +0300

    initialized eureka discovery server

---
+++ eureka-discovery/src/main/java/ru/thesis/discovery/EurekaDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package ru.thesis.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryApplication.class, args);
+	}
+
+}
